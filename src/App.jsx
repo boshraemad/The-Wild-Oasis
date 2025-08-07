c3369de508bdbc5fc9a780ce7eb32e4e029ac95b
boshraemad <boshraemad2004@icloud.com>
2025-08-07 20:52:44 +0300

    setup react-query && get cabins

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import GlobalStyles from "./styles/GlobalStyles";
 import { BrowserRouter , Route , Routes , Navigate } from "react-router-dom";
+import { QueryClient , QueryClientProvider} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Dashboard from "./pages/Dashboard";
 import Cabins from "./pages/Cabins";
 import Bookings from "./pages/Bookings";
@@ -9,9 +11,22 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 
+
+//create react query client
+
+const queryClient = new QueryClient({
+  defaultOption:{
+    queries:{
+      staleTime: 0
+    }
+  }
+})
 function App() {
   return (
-    <BrowserRouter>
+ <>
+ <QueryClientProvider client={queryClient}>
+  <ReactQueryDevtools initialIsOpen={false}/>
+ <BrowserRouter>
     <GlobalStyles/>
     <Routes>
       <Route path="/" element={<AppLayout/>}>
@@ -27,6 +42,8 @@ function App() {
       <Route path="*" element={<PageNotFound/>}/>
     </Routes>
     </BrowserRouter>
+ </QueryClientProvider>
+ </>
   )
 }
 
