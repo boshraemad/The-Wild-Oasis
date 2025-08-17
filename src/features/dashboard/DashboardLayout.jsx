bfb36dd9b23dc0e4e2163b1674b184d9506fe901
boshraemad <boshraemad2004@icloud.com>
2025-08-18 02:56:17 +0300

    dashboard : Compute recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,23 @@
 import styled from "styled-components";
-
+import useRecentBookings from "../dashboard/useRecentBookings";
+import useRecentStays from "./useRecentStays";
+import Spinner from "../../ui/Spinner"
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+
+export default function DashboardLayout({children}) {
+  const {bookings , isLoading:isLoading1} = useRecentBookings();
+  const {recentStays , stays , isLoading:isLoading2} = useRecentStays();
+  console.log(bookings , stays , recentStays)
+  if(isLoading1 || isLoading2) return;
+  return (
+    <StyledDashboardLayout>
+      {children}
+    </StyledDashboardLayout>
+  )
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,19 @@
+import DashboardLayout from "../features/dashboard/DashboardLayout";
+import DashboardFilter from "../features/dashboard/DashboardFilter";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Dashboard() {
   return (
-    <Row type="horizontal">
+  <>
+      <Row type="horizontal">
       <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
+      <DashboardFilter/>
     </Row>
+    <DashboardLayout>
+      <div>mmm</div>
+    </DashboardLayout>
+  </>
   );
 }
 
